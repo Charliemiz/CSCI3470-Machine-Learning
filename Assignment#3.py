03f70014878e3ad93e8043e1049ecb9a5c68563c
Charliemiz <cdmisbach@icloud.com>
2025-03-24 14:50:48 -0500

    start assignment #2

---
+++ Assignment#3.py
@@ -0,0 +1,7 @@
+# Charlie Misbach
+# Machine Learning - CSCI3470
+# Assignment #3
+# Due: 4/7/25
+
+# Question #1: Implement a convolutional Autoencoder and discuss your 
+# discovery/thinking during the implementation.
